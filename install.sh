5ca2d47611394ca9306774392e9265e6234e1323
Ray Xu <ritchxu@github.com>
2020-05-21 17:34:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
-git config --global user.name "Ryan Simmen"
-git config --global user.email "ryansimmen@github.com"
+git config --global user.name "ritchxu"
+git config --global user.email "ritchxu@github.com"
 git config --global pager.branch false
 git config --global push.default current
 
-mkdir -p ~/.vim/colors
-cp ~/.dotfiles/.vim/colors/molokai.vim ~/.vim/colors/
 ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.vimrc ~/
-
-# vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +qall
 
 # zsh 
 if [ "$SHELL" != "/usr/bin/zsh" ]; then
