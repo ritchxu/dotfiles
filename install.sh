1a13102fb4465899d79670dc8abc16d32f5b59e6
Ray Xu <ritchxu@github.com>
2026-03-24 22:17:39 -0700

    .

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ git config --global push.default current
 
 # oh-my-zsh
 sudo apt-get update && sudo apt-get install -y zsh
+rm -rf /home/vscode/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
